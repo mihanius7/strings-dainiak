f54ab8ddf1c285a543ced95f0e1a229322d64220
Mihas Dainiak <mihanius@gmail.com>
2022-03-28 17:42:05 +0300

    StringFragment class using changed

--- strings-dainiak/src/com/belhard/strings/Task6.java
+++ strings-dainiak/src/com/belhard/strings/Task6.java
@@ -9,7 +9,7 @@ public class Task6 {
  public static void main(String[] args) {
 		String targetString = STRING_CASES[3];
 		System.out.printf("Test case: %n%s%n", targetString);
-		System.out.printf("%nMax spaces count: %d", StringUtil.defineMaxCharInARow(targetString, " ").maxCharsCount);
+		System.out.printf("%nMax spaces count: %d", StringUtil.defineMaxCharInARow(targetString, " ").charsCount);
 	}	
 
 }
