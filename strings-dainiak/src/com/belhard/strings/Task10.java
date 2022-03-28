f547a559b24f373f0a0dd443b93198c49fb01430
Mihas Dainiak <mihanius@gmail.com>
2022-03-28 17:43:13 +0300

    Task10 completed by extending existing StringFragment class

--- strings-dainiak/src/com/belhard/strings/Task10.java
+++ strings-dainiak/src/com/belhard/strings/Task10.java
@@ -3,11 +3,13 @@ package com.belhard.strings;
 import static com.belhard.strings.DemoStrings.STRING_CASES;
 
 import com.belhard.util.StringUtil;
+import com.belhard.util.StringUtil.StringFragment;
 
 public class Task10 {
 	public static void main(String[] args) {
 		String targetString = STRING_CASES[1];
-		System.out.printf("Test case: %n%s", targetString);
-		System.out.printf("%nThe longest word length is: %d%n%n", StringUtil.defineMaxCharInARow(targetString, "\\w+").maxCharsCount);
+		System.out.printf("Test case: " + targetString);
+		StringFragment longestWord = StringUtil.defineMaxCharInARow(targetString, "\\w+");
+		System.out.printf("%n%nThe longest word is: '%s', length %d", longestWord.content, longestWord.charsCount);
 	}	
 }
