ffa49619a7820c23150b5cbe7ab46e916cd74b40
Mihas Dainiak <mihanius@gmail.com>
2022-03-29 10:03:35 +0300

    StringUtil.LOWER_LETTER was renamed

--- strings-dainiak/src/com/belhard/strings/Task11.java
+++ strings-dainiak/src/com/belhard/strings/Task11.java
@@ -9,8 +9,8 @@ public class Task11 {
 	public static void main(String[] args) {
 		String targetString = STRING_CASES[1];
 		System.out.printf("Test case: %n" + targetString);
-		System.out.printf("%n%nMax lower case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.LOWER_LETTERS, false));
-		System.out.printf("%nMax upper case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.UPPER_LETTERS, false));
+		System.out.printf("%n%nMax lower case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.LOWER_LETTER, false));
+		System.out.printf("%nMax upper case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.UPPER_LETTER, false));
 	}	
 
 }
