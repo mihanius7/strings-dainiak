2c85dd1ad17db5583105b8320b4951a75d9a6cde
Mihas Dainiak <mihanius@gmail.com>
2022-03-29 09:44:07 +0300

    Task11, part2 completed

--- strings-dainiak/src/com/belhard/strings/Task11.java
+++ strings-dainiak/src/com/belhard/strings/Task11.java
@@ -7,9 +7,10 @@ import com.belhard.util.StringUtil;
 public class Task11 {
 
 	public static void main(String[] args) {
-		String targetString = STRING_CASES[2];
-		System.out.printf("Test case: %n%s%n", targetString);
-		System.out.printf("%nMax lower case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.LOWER_LETTERS, true));
+		String targetString = STRING_CASES[1];
+		System.out.printf("Test case: %n" + targetString);
+		System.out.printf("%n%nMax lower case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.LOWER_LETTERS, false));
+		System.out.printf("%nMax upper case letters count: %d", StringUtil.countCoincidencies(targetString, StringUtil.UPPER_LETTERS, false));
 	}	
 
 }
